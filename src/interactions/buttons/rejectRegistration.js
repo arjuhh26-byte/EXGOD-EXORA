0afa948b4bef7f77a0295fc8e30e7c5076e3840c
arjuhh26-byte <arjuhh26@gmail.com>
2026-06-20 14:32:32 +0530

    test

--- src/interactions/buttons/rejectRegistration.js
+++ src/interactions/buttons/rejectRegistration.js
@@ -2,24 +2,14 @@ import {
 ModalBuilder,
 TextInputBuilder,
 TextInputStyle,
-ActionRowBuilder
+ActionRowBuilder,
+PermissionFlagsBits
 } from "discord.js";
 
 export default {
 name: "reject_registration",
 
 async execute(interaction) {
-
-if (
-  !interaction.member.permissions.has(
-    PermissionFlagsBits.Administrator
-  )
-) {
-  return interaction.reply({
-    content: "❌ You don't have permission to use this button.",
-    ephemeral: true
-  });
-}
 
 const modal = new ModalBuilder()
   .setCustomId("reject_modal")
